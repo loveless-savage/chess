c51eeaaf86934bcf67243ba899ebc914a1819660
loveless-savage <titansofalgenia@gmail.com>
2025-09-11 21:21:08 -0600

    ChessBoard pieces switched from Map to ChessPiece array

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -11,38 +11,38 @@ import java.util.HashMap;
  */
 public class ChessBoard {
 
-    private Map<ChessPosition,ChessPiece> pieces;
+    private ChessPiece[][] pieces;
 
     public ChessBoard() {
-        pieces = new HashMap<>();
+        pieces = new ChessPiece[8][8];
     }
 
     /**
      * Adds a chess piece to the chessboard
      *
-     * @param position where to add the piece to
+     * @param pos where to add the piece to
      * @param piece    the piece to add
      */
-    public void addPiece(ChessPosition position, ChessPiece piece) {
-        pieces.put(position,piece);
+    public void addPiece(ChessPosition pos, ChessPiece piece) {
+        pieces[pos.getRow()-1][pos.getColumn()-1] = piece;
     }
 
     /**
      * Gets a chess piece on the chessboard
      *
-     * @param position The position to get the piece from
+     * @param pos The position to get the piece from
      * @return Either the piece at the position, or null if no piece is at that
      * position
      */
-    public ChessPiece getPiece(ChessPosition position) {
-        return pieces.get(position);
+    public ChessPiece getPiece(ChessPosition pos) {
+        return pieces[pos.getRow()-1][pos.getColumn()-1];
     }
 
     /**
      * Sets the board to the default starting board
      */
     public void resetBoard() {
-        pieces.clear();
+        //pieces.clear();
         loadBoard("""
                 |r|n|b|q|k|b|n|r|
                 |p|p|p|p|p|p|p|p|
@@ -56,7 +56,7 @@ public class ChessBoard {
     }
 
     /**
-     * convert ascii representation of board to object representation
+     * mappings between ascii representation of pieces to object representation
      */
     private static final Map<Character, ChessPiece.PieceType> CHAR_TO_TYPE_MAP = Map.of(
             'p', ChessPiece.PieceType.PAWN,
@@ -65,36 +65,6 @@ public class ChessBoard {
             'q', ChessPiece.PieceType.QUEEN,
             'k', ChessPiece.PieceType.KING,
             'b', ChessPiece.PieceType.BISHOP);
-    private void loadBoard(String boardText) {
-        Map<ChessPosition, ChessPiece> piecesTemp = new HashMap<>();
-        int row = 8;
-        int column = 1;
-        for (var c : boardText.toCharArray()) {
-            switch (c) {
-                case '\n' -> {
-                    column = 1;
-                    row--;
-                }
-                case ' ' -> column++;
-                case '|' -> {
-                }
-                default -> {
-                    ChessGame.TeamColor color = Character.isLowerCase(c) ? ChessGame.TeamColor.BLACK
-                            : ChessGame.TeamColor.WHITE;
-                    var type = CHAR_TO_TYPE_MAP.get(Character.toLowerCase(c));
-                    var position = new ChessPosition(row, column);
-                    var piece = new ChessPiece(color, type);
-                    piecesTemp.put(position, piece);
-                    column++;
-                }
-            }
-        }
-        pieces = piecesTemp;
-    }
-
-    /**
-     * convert object representation of board to ascii representation
-     */
     private static final Map<ChessPiece.PieceType, Character> TYPE_TO_CHAR_MAP = Map.of(
             ChessPiece.PieceType.PAWN, 'p',
             ChessPiece.PieceType.KNIGHT, 'n',
@@ -102,12 +72,46 @@ public class ChessBoard {
             ChessPiece.PieceType.QUEEN, 'q',
             ChessPiece.PieceType.KING, 'k',
             ChessPiece.PieceType.BISHOP, 'b');
+
+    /**
+     * convert ascii representation of board to object representation
+     */
+    private void loadBoard(String boardText) {
+        char[] boardIn = boardText.toCharArray();
+        int row = 8;
+        int col = 1;
+        for (var c : boardIn){
+            if(c == '|') continue; // skip delimiters
+            if(c == '\n') { // new row
+                row--;
+                col=1;
+                continue;
+            }
+            if(c == ' ') pieces[8-row][col-1] = null; // empty spaces
+            if(pieces[8-row][col-1] != null &&
+                    c == TYPE_TO_CHAR_MAP.get(pieces[8-row][col-1].getPieceType())) { // skip unchanged pieces
+                continue;
+            }
+
+            ChessGame.TeamColor color = Character.isLowerCase(c) ? ChessGame.TeamColor.BLACK
+                    : ChessGame.TeamColor.WHITE;
+            var type = CHAR_TO_TYPE_MAP.get(Character.toLowerCase(c));
+            pieces[8-row][col-1] = new ChessPiece(color,type);
+            col++;
+        }
+    }
+
+    /**
+     * convert object representation of board to ascii representation
+     */
     public String printBoard() {
         char[] boardText = new char[144];
+        /*
         int i,j;
         for (i = 0; i < 8; i++) {
             for (j = 0; j < 8; j++) {
                 boardText[18*i+2*j] = '|';
+                chess.ChessPiece currentPiece = pieces[7-i][j];
                 boardText[18*i+2*j+1] = ' ';
             }
             boardText[18*i+16] = '|';
@@ -124,6 +128,7 @@ public class ChessBoard {
             j =-1+pos.getColumn();
             boardText[18*i+2*j+1] = pieceT;
         }
+        */
         return new String(boardText);
     }
 
