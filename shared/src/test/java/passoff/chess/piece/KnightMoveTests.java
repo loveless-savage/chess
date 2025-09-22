5e4ae578e6ac2dbb3e28c15f7824d42ba9bb02e1
loveless-savage <titansofalgenia@gmail.com>
2025-09-22 14:28:28 -0600

    test folder restructure: piecemoves

--- shared/src/test/java/passoff/chess/piece/KnightMoveTests.java
+++ shared/src/test/java/passoff/chess/piece/KnightMoveTests.java
@@ -1,4 +1,4 @@
-package passoff.chess.piecemoves;
+package passoff.chess.piece;
 
 import chess.ChessPosition;
 import org.junit.jupiter.api.Test;
--- shared/src/test/java/passoff/chess/piece/PawnMoveTests.java
+++ shared/src/test/java/passoff/chess/piece/PawnMoveTests.java
@@ -1,4 +1,4 @@
-package passoff.chess.piecemoves;
+package passoff.chess.piece;
 
 import chess.ChessMove;
 import chess.ChessPiece;
