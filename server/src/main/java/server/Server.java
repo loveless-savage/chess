fb103ccd6f68dbe18c3e9948daba94048351d006
loveless-savage <titansofalgenia@gmail.com>
2025-10-18 16:43:40 -0600

    server accepts request bodies and returns valid data

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,23 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
 
 public class Server {
 
     private final Javalin javalin;
+    private final Gson serializer;
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        serializer = new Gson();
 
-        // Register your endpoints and exception handlers here.
-
+        javalin.delete("/db", ctx -> {
+            System.out.println(ctx.body().getClass());
+            ctx.status(418);
+            ctx.contentType("text/plain");
+            ctx.result(serializer.toJson("heey"));
+        });
     }
 
     public int run(int desiredPort) {
