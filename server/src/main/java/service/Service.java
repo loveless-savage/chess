f9f44fc71ad081e90701b896382269b0a5b31242
loveless-savage <titansofalgenia@gmail.com>
2025-10-18 20:58:17 -0600

    Service methods listed for prototyping

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,5 +1,6 @@
 package service;
 
+import chess.*;
 import dataaccess.*;
 import model.*;
 
@@ -17,5 +18,23 @@ public class Service {
     public AuthData register(UserData newUser) { // TODO: result instead of AuthData
         userDAO.create(newUser);
         return new AuthData(newUser.username(),"fillerauth");
+    }
+    public AuthData login(AuthData usernameAndPassword) {
+        return usernameAndPassword;
+    }
+    public void logout(String authToken) {
+        return;
+    }
+    public GameData[] listGames(String authToken) {
+        return null;
+    }
+    public int createGame(String authToken, String gameName) {
+        return 0;
+    }
+    public void joinGame(String authToken, ChessGame.TeamColor playerColor, int gameID) {
+        return;
+    }
+    public void clear() {
+        return;
     }
 }
