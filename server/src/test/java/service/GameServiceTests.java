80892731c2f03e2e20e75209af752b0c5fd95d0a
loveless-savage <titansofalgenia@gmail.com>
2025-10-18 22:56:19 -0600

    test files generated

---
+++ server/src/test/java/service/GameServiceTests.java
@@ -0,0 +1,18 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+public class GameServiceTests {
+    @Test
+    public void listGamesTest() {
+    }
+    @Test
+    public void createGameTest() {
+    }
+    @Test
+    public void joinGameTest() {
+    }
+    @Test
+    public void clearTest() {
+    }
+}
---
+++ server/src/test/java/service/UserServiceTests.java
@@ -0,0 +1,18 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTests {
+    @Test
+    public void registerTest() {
+    }
+    @Test
+    public void loginTest() {
+    }
+    @Test
+    public void logoutTest() {
+    }
+    @Test
+    public void clearTest() {
+    }
+}
