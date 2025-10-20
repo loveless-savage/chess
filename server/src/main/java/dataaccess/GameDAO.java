a0917536344f3075e18e415497619899e58e798a
loveless-savage <titansofalgenia@gmail.com>
2025-10-19 18:50:10 -0600

    GameDAO.list() added

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -3,7 +3,7 @@ package dataaccess;
 import model.*;
 
 public class GameDAO extends MemoryDAO<GameData,Integer>{
-    //public UserData[] list(String authToken) {
-        //return new UserData[]{refdata};
-    //}
+    public GameData[] list() {
+        return new GameData[]{refdata};
+    }
 }
