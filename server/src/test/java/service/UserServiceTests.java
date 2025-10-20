73c5b65afb818968ddb153a661a6d959331be18a
loveless-savage <titansofalgenia@gmail.com>
2025-10-19 20:31:08 -0600

    UserServiceTests added

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,18 +1,74 @@
 package service;
 
-import org.junit.jupiter.api.Test;
+import model.*;
+import org.junit.jupiter.api.*;
 
 public class UserServiceTests {
+    private static UserService userService;
+    private static UserData goodData, otherData;
+    private static AuthData loginRequest;
+
+    @BeforeAll
+    public static void init(){
+        userService = new UserService();
+        goodData = new UserData("correctUsername","correctPassword","correct@email");
+        otherData = new UserData("otherUsername","otherPassword","other@email");
+        loginRequest = new AuthData("correctUsername","correctPassword");
+    }
+    @BeforeEach
+    public void setup() {
+        userService.register(goodData);
+    }
+    @AfterEach
+    public void takeDown() {
+        userService.clear();
+    }
+
     @Test
     public void registerTest() {
+        userService.clear();
+        Assertions.assertEquals("correctUsername", userService.register(goodData).username());
+        Assertions.assertEquals("otherUsername", userService.register(otherData).username());
     }
+    @Test
+    public void registerAlreadyTakenTest() {
+        Assertions.assertThrows(AlreadyTakenException.class,() -> userService.register(goodData));
+    }
+
     @Test
     public void loginTest() {
+        Assertions.assertEquals(loginRequest.username(), userService.login(loginRequest).username());
     }
+    @Test
+    public void loginUserNotFoundTest() {
+        AuthData badLoginRequest = new AuthData("badUsername","correctPassword");
+        Assertions.assertThrows(NotFoundException.class,() -> userService.login(badLoginRequest));
+    }
+    @Test
+    public void loginBadPasswordTest() {
+        AuthData badLoginRequest = new AuthData("correctUsername","badPassword");
+        Assertions.assertThrows(UnauthorizedException.class,() -> userService.login(badLoginRequest));
+    }
+
     @Test
     public void logoutTest() {
+        String goodToken = userService.login(loginRequest).authToken();
+        userService.logout(goodToken);
+        Assertions.assertThrows(UnauthorizedException.class,() -> userService.logout(goodToken));
     }
     @Test
+    public void logoutUnauthorizedTest() {
+        String badToken = userService.login(loginRequest).authToken() + "_bad";
+        Assertions.assertThrows(UnauthorizedException.class,() -> userService.logout(badToken));
+    }
+
+    @Test
     public void clearTest() {
+        userService.register(otherData);
+        userService.clear();
+        AuthData loginRequest = new AuthData("correctUsername","correctPassword");
+        Assertions.assertThrows(NotFoundException.class,() -> userService.login(loginRequest));
+        AuthData otherLoginRequest = new AuthData("otherUsername","otherPassword");
+        Assertions.assertThrows(NotFoundException.class,() -> userService.login(otherLoginRequest));
     }
 }
