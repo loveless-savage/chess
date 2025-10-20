2b47e5ddfeaaa4d6d218cf15ddc1042c336b6dda
loveless-savage <titansofalgenia@gmail.com>
2025-10-19 21:27:12 -0600

    GameDAO tracks gameIDs and provides an available one on request

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -3,6 +3,20 @@ package dataaccess;
 import model.*;
 
 public class GameDAO extends MemoryDAO<GameData,Integer>{
+    private int nextID = 0;
+
+    public void create(GameData data) {
+        super.create(data);
+        nextID++;
+    }
+    public int getNextID() {
+        return nextID;
+    }
+    public void clear() {
+        super.clear();
+        nextID = 0;
+    }
+
     public GameData[] list() {
         return refdata.values().toArray(GameData[]::new);
     }
