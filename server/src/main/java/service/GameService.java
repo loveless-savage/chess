4a852c3f680111f305b58cd5733b3aef279ea2cd
loveless-savage <titansofalgenia@gmail.com>
2025-10-19 23:49:12 -0600

    GameService passes all tests

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -18,27 +18,42 @@ public class GameService {
     }
 
     public GameData[] listGames(String authToken) {
-        authDAO.get(authToken); // FIXME: UnauthorizedException [401]
+        if(authDAO.get(authToken) == null) { // TODO: UnauthorizedException [401]
+            throw new UnauthorizedException("unauthorized");
+        }
         return gameDAO.list();
     }
     public int createGame(String authToken, String gameName) {
-        authDAO.get(authToken); // FIXME: UnauthorizedException [401]
+        if(authDAO.get(authToken) == null) { // TODO: UnauthorizedException [401]
+            throw new UnauthorizedException("unauthorized");
+        }
         int newGameID = gameDAO.getNextID();
         GameData newGame = new GameData(newGameID,"","",gameName,new ChessGame());
         gameDAO.create(newGame);
         return newGameID;
     }
     public void joinGame(String authToken, ChessGame.TeamColor playerColor, int gameID) {
-        AuthData userAuth = authDAO.get(authToken); // FIXME: UnauthorizedException [401]
-        GameData currentGame = gameDAO.get(gameID); // FIXME: NotFoundException [404]
+        AuthData userAuth = authDAO.get(authToken);
+        if(userAuth == null) { // TODO: UnauthorizedException [401]
+            throw new UnauthorizedException("unauthorized");
+        }
+        GameData currentGame = gameDAO.get(gameID);
+        if(currentGame == null) { // TODO: NotFoundException [404]
+            throw new NotFoundException("game not found");
+        }
         GameData updatedGame;
         if(playerColor == ChessGame.TeamColor.WHITE) {
-            // TODO: AlreadyTakenException [403]
+            if(!currentGame.whiteUsername().isEmpty()) { // TODO: AlreadyTakenException [403]
+                throw new AlreadyTakenException("already taken");
+            }
             updatedGame = new GameData(currentGame.gameID(),
                                        userAuth.username(), currentGame.blackUsername(),
                                        currentGame.gameName(),
                                        currentGame.game());
         } else {
+            if(!currentGame.blackUsername().isEmpty()) { // TODO: AlreadyTakenException [403]
+                throw new AlreadyTakenException("already taken");
+            }
             updatedGame = new GameData(currentGame.gameID(),
                                        currentGame.whiteUsername(), userAuth.username(),
                                        currentGame.gameName(),
@@ -46,6 +61,7 @@ public class GameService {
         }
         gameDAO.update(updatedGame);
     }
+
     public void clear() {
         authDAO.clear();
         gameDAO.clear();
