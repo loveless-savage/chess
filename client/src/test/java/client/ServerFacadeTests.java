8dcb83531bad8e574300acff896e0f03f93500a0
loveless-savage <titansofalgenia@gmail.com>
2025-11-09 22:36:33 -0700

    ServerFacadeTests basic structure

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -5,7 +5,7 @@ import server.Server;
 
 
 public class ServerFacadeTests {
-
+    private static ServerFacade facade;
     private static Server server;
 
     @BeforeAll
@@ -13,17 +13,42 @@ public class ServerFacadeTests {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("localhost",port);
     }
-
+    @BeforeEach
+    public void setup() {
+        facade.clear();
+    }
     @AfterAll
-    static void stopServer() {
+    static void stop() {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void clearTest() {
     }
 
+    @Test
+    public void registerTest() {
+    }
+
+    @Test
+    public void loginTest() {
+    }
+
+    @Test
+    public void logoutTest() {
+    }
+
+    @Test
+    public void listGamesTest() {
+    }
+
+    @Test
+    public void createGameTest() {
+    }
+
+    @Test
+    public void joinGameTest() {
+    }
 }
