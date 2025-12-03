505010e112dd316176e56d8303211f75ceaacbd4
loveless-savage <titansofalgenia@gmail.com>
2025-12-03 13:31:13 -0700

    WebsocketCommunicator created

---
+++ client/src/main/java/client/WebsocketCommunicator.java
@@ -0,0 +1,26 @@
+package client;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+
+public class WebsocketCommunicator {
+    public Session session;
+    public WebsocketCommunicator(String hostIn, int portIn) throws Exception {
+        URI uri = new URI(String.format("ws://%s:%d/ws",hostIn,portIn));
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        session = container.connectToServer(this, uri);
+        session.addMessageHandler(new MessageHandler.Whole<String>() {
+            public void onMessage(String message) {
+                System.out.println(message);
+            }
+        });
+    }
+
+    public void send(String message) throws IOException {
+        session.getBasicRemote().sendText(message);
+    }
+
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+}
