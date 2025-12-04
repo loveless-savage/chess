7387b175e2064b51b52f45589f0048289f0bd823
loveless-savage <titansofalgenia@gmail.com>
2025-12-04 16:04:23 -0700

    PlayService parses user game commands and catches parsing errors

--- server/src/main/java/service/PlayService.java
+++ server/src/main/java/service/PlayService.java
@@ -1,22 +1,44 @@
 package service;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import io.javalin.websocket.*;
 import org.jetbrains.annotations.NotNull;
+import chess.*;
+import websocket.commands.*;
 import websocket.messages.*;
 
 public class PlayService implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
     @Override
     public void handleConnect(@NotNull WsConnectContext ctx) {
         ctx.enableAutomaticPings();
-        System.out.println("Websocket connected");
+        System.out.println("Websocket user connected");
     }
 
     @Override
     public void handleMessage(@NotNull WsMessageContext ctx) {
         System.out.println("Received message " + ctx.message());
-        NotificationMessage msg = new NotificationMessage(ctx.message());
-        ctx.send(new Gson().toJson(msg));
+        UserGameCommand cmd;
+        try {
+            cmd = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+        } catch (JsonSyntaxException e) {
+            ErrorMessage msg = new ErrorMessage("ERROR: command cannot be parsed");
+            ctx.send(new Gson().toJson(msg));
+            return;
+        }
+        switch (cmd.getCommandType()) {
+            case CONNECT -> {
+                System.out.println("Connecting user "+cmd.getAuthToken()+" to game "+cmd.getGameID()); // FIXME
+                LoadGameMessage msg = new LoadGameMessage(new ChessGame()); // FIXME
+                ctx.send(new Gson().toJson(msg));
+                // TODO: notify others
+            }
+            case MAKE_MOVE -> {
+                cmd = new Gson().fromJson(ctx.message(),MakeMoveCommand.class);
+            }
+            case LEAVE -> {}
+            case RESIGN -> {}
+        }
     }
 
     @Override
